b2029de7703d992b4647cef4da7a2abaf380c881
Krish Kapila <krish@cario.com>
2025-06-18 20:14:25 -0500

    This is a revision that ChatGPT edited using my existing code to make the program more efficient.

--- src/greetingApp.java
+++ src/greetingApp.java
@@ -3,17 +3,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class greetingApp {
-    public static void main(String [] args) {
-        Scanner scanner = new Scanner (System.in); //reads user input
-        System.out.println("Hey! What is your name:  ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hey! What is your name: ");
+        String name = scanner.nextLine();
 
-        String name = scanner.nextLine(); //creates a variable for the name to be used later
-
-        System.out.println("Hello " + name + "! " + "It is so nice to meet you.");
+        System.out.println("Hello " + name + "! It is so nice to meet you.");
 
         LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
-
         System.out.println("The current date and time is " + currentDateTime.format(formatter) + ".");
     }
 }
