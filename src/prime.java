57d088cf5449dd9626ee7c6d65af23cbe2b844d2
Krish Kapila <krish@cario.com>
2025-06-18 20:16:26 -0500

    This is a revision that ChatGPT edited using my existing code to make the program more efficient.

--- src/prime.java
+++ src/prime.java
@@ -2,27 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class prime {
-
-    public static void main (String [] args) {
+    public static void main(String[] args) {
         List<Integer> primeNumbers = new ArrayList<>();
 
-        for(int numCheck = 2; numCheck <= 100; numCheck++) {
-            boolean isPrime = true;
-
-            for (int factor = 2; factor <= numCheck/2; factor++) {
-                if(numCheck % factor == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-
-            if(isPrime) {
+        for (int numCheck = 2; numCheck <= 100; numCheck++) {
+            if (isPrime(numCheck)) {
                 primeNumbers.add(numCheck);
             }
         }
-        System.out.println("The prime numbers 1 through 100 are: ");
-        for(int number : primeNumbers) {
+
+        System.out.println("The prime numbers from 1 through 100 are:");
+        for (int number : primeNumbers) {
             System.out.println(number);
         }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
     }
 }
