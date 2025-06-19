892facc1724f1c504d7cc1151f337c002d462269
Krish Kapila <krish@cario.com>
2025-06-18 20:15:55 -0500

    This is a revision that ChatGPT edited using my existing code to make the program more efficient.

--- src/palindrome.java
+++ src/palindrome.java
@@ -2,30 +2,29 @@ import java.util.Scanner;
 
 public class palindrome {
 
-    public static void main (String [] args) {
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the string you want to check: ");
+        System.out.print("Enter the string you want to check: ");
         String input = scanner.nextLine();
         String inputLower = input.toLowerCase();
 
         int start = 0;
-        int end = input.length() - 1;
+        int end = inputLower.length() - 1;
         boolean isPalindrome = true;
 
-        while(start < end) {
-            if(inputLower.charAt(start) != inputLower.charAt(end)) { //if the letter at start is not equal to letter at end
+        while (start < end) {
+            if (inputLower.charAt(start) != inputLower.charAt(end)) {
                 isPalindrome = false;
-                break; //breaks the loop
+                break;
             }
-
             start++;
             end--;
         }
-        if(isPalindrome) {
-            System.out.println("Your string " + input + " is a palindrome!");
+
+        if (isPalindrome) {
+            System.out.println("Your string \"" + input + "\" is a palindrome!");
         } else {
-            System.out.println("Your string " + input + " is not a palindrome.");
+            System.out.println("Your string \"" + input + "\" is not a palindrome.");
         }
     }
 }
